77c389c6e29445620d7419a5e21aaade1181e423
saferodrigoestacio <202501270689@alunos.estacio.br>
2025-04-25 01:05:01 +0000

    ajustes

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,14 +3,17 @@
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 
+#define LINHA 10
+#define COLUNA 10
+
 int main() {
     // Inicializa o tabuleiro de batalha naval
-    char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // Inicializa as colunas que representam o tabuleiro
-    int tabuleiro[10][10]; // Inicializa o tabuleiro com 0 (vazio)
+    char linha[COLUNA] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // Inicializa as linhas que representam o tabuleiro
+    int tabuleiro[LINHA][COLUNA]; // Inicializa o tabuleiro com 0 (vazio)
 
     // Utilize um loop para preencher o tabuleiro com 0s
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++) {
+    for(int i = 0; i < LINHA; i++) {
+        for(int j = 0; j < COLUNA; j++) {
             tabuleiro[i][j] = 0;
         }
     }
@@ -32,31 +35,31 @@ int main() {
 
     // Navio 3 (diagonal)
     for (int i = 1; i < 4; i++) {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < COLUNA; j++) {
             tabuleiro[i][i + 5] = 3;
         }          
     }
     
     // Navio 4 (diagonal)
     for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < COLUNA; j++) {
             tabuleiro[i][2 - i] = 3;
         }          
     }
 
     printf("Tabuleiro Inicial:\n");
-    // Exibe as colunas A, B, C, D, E, F, G, H, I, J
+    // Exibe as linha A, B, C, D, E, F, G, H, I, J
     printf("   ");
-    for(int i = 0; i < 10; i++) {
-        printf("%c ", colunas[i]);
+    for(int i = 0; i < LINHA; i++) {
+        printf("%c ", linha[i]);
     }
     printf("\n");
 
     // Exibe o tabuleiro
-    for(int i = 0; i < 10; i++) {
-        printf("%2d ", i + 1); // Exibe os números de 1 a 10 para as colunas
+    for(int i = 0; i < LINHA; i++) {
+        printf("%2d ", i + 1); // Exibe os números de 1 a 10 para as linhas
 
-        for(int j = 0; j < 10; j++) {
+        for(int j = 0; j < COLUNA; j++) {
             printf("%d ", tabuleiro[i][j]); // Exibe o valor do tabuleiro (0 ou 3)
         }
         printf("\n");
